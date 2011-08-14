35f06cd67bb809f9c5b1968574fb37b9753c1471
matthew tullett <matt@twelve20.co.uk>
2011-08-14 10:06:43 +0100

    Amended $confgi array to with a prefix of ci_

--- config/cmonitor.php
+++ config/cmonitor.php
@@ -6,10 +6,10 @@
 |--------------------------------------------------------------------------
 |
 */
-$config['api_key']  = 'YOUR API KEY HERE';
-$config['protocol'] = 'https';
-$config['debug_level'] = 0;
-$config['host'] = 'api.createsend.com';
-$config['log'] = NULL;
-$config['serialiser'] = NULL;
-$config['transport'] = NULL;
+$config['ci_api_key']  = 'YOUR API KEY HERE';
+$config['ci_protocol'] = 'https';
+$config['ci_debug_level'] = 0;
+$config['ci_host'] = 'api.createsend.com';
+$config['ci_log'] = NULL;
+$config['ci_serialiser'] = NULL;
+$config['ci_transport'] = NULL;
